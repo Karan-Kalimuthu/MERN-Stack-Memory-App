00b6efbfb269f533cf1ac52e116c2176b0bb8151
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-03-13 15:51:43 +0530

    Back-End GET and POST implemented

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,5 +4,6 @@ const postController = require('../controllers/posts');
 
 //localhost:5000/posts
 router.get('/', postController.getPosts);
+router.post('/', postController.createPost);
 
 module.exports = router;
